a8b63b784562e20a177473d849cd5189b6dc9479
jassmarvel <159693800+jassmarvel@users.noreply.github.com>
2024-02-22 22:49:13 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/jassmarvel/RequestToJoin3 /RequestToJoin3
+  git clone https://github.com/jassmarvel/RequestToJoinnn /RequestToJoinnn
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /RequestToJoin3
+  git clone $UPSTREAM_REPO /RequestToJoinnn
 fi
-cd /RequestToJoin3
+cd /RequestToJoinnn
 pip3 install -U -r requirements.txt
 echo "Bot Started...."
 python3 bot.py
