8aeb21e32573cf86b01d1e6c202518c1fae9874b
jassmarvel <159693800+jassmarvel@users.noreply.github.com>
2024-02-22 22:45:03 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Vijay14958/RequestToJoin3 /RequestToJoin3
+  git clone https://github.com/jassmarvel/RequestToJoin3 /RequestToJoin3
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /RequestToJoin3
